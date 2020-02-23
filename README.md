240a13774882fbe514102b4d9e6b25a496402c5c
danielroa98 <daniel.roa98@gmail.com>
2020-02-23 15:20:36 -0600

    Ya lee documentos .txt

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ---
 
 ##### Integrantes:
-1. *Luis Daniel Roa González* - *[A01021960]* - *[Poner aquí su campus]*
+1. *Luis Daniel Roa González* - *A01021960* - *Campus Santa Fe*
 2. *[Poner aquí Nombre y Apellidos del integrante 2]* - *[Poner aquí su Matrícula]* - *[Poner aquí su campus]*
 3. *[Poner aquí Nombre y Apellidos del integrante 3]* - *[Poner aquí su Matrícula]* - *[Poner aquí su campus]*
 4. *[Poner aquí Nombre y Apellidos del integrante 4]* - *[Poner aquí su Matrícula]* - *[Poner aquí su campus]*
--- sources/Tarea1.cpp
+++ sources/Tarea1.cpp
@@ -5,4 +5,30 @@
  * 
  */
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    fstream archivoPrueba;
+    string nombreArchivo, demo;
+
+    cout << "A continuación ingrese el nombre del documento\nDebe de tener la terminación .txt" << endl;
+
+    cin >> nombreArchivo;
+
+    cout << "\nSe ingresó el documento con nombre " << nombreArchivo << "\n" << endl;
+
+    archivoPrueba.open(nombreArchivo.c_str());
+
+    while (archivoPrueba >> demo)
+    {
+        cout << demo << endl;
+    }
+    
+
+    return 0;
+}
