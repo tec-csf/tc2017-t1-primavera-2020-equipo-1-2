6ff19ca450d197e08cdd0900032966c7523864ce
KatiaBellido <42215148+KatiaBellido@users.noreply.github.com>
2020-03-10 12:47:17 -0600

    Update hello.py

--- sources/hello.py
+++ sources/hello.py
@@ -1,10 +1,4 @@
-import sys
-import sympy
-
-lista = sys.argv
-
-with open('polinomio.txt', 'r') as myfile:
-  data = myfile.read()
-
-sol = sympy.expand(data)
-print(sol)
+print("Hola Kat")
+f=open('file.txt','w')
+f.write("Hola =)")
+f.close()
