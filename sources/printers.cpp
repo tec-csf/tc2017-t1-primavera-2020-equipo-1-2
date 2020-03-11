10587561eff6958aabe736e467f8eeb2e564a707
Katia  Bellido <A01023638@itesm.mx>
2020-03-10 20:02:14 -0600

    arreglando tabla

--- sources/printers.cpp
+++ sources/printers.cpp
@@ -15,14 +15,14 @@ void PrintTable(vector<string> vecComp, vector<int> contOE, int maxLinea)
 
     //table formatting
     cout <<setfill(' ') << setw(0) << "No. de linea|" << setfill(' ') << setw((maxLinea+6)/2) << "Código";
-    cout<< setfill(' ') << setw(maxLinea/2+10)<<"|"<<setfill(' ') << setw(10)<< "OE" <<setfill(' ') << setw(10)  <<"|"<< setfill(' ') << setw(20) <<"Polinomio|"<< endl;
+    cout<< setfill(' ') << setw(maxLinea/2+10)<<"|"<<setfill(' ') << setw(4)<< "OE" <<setfill(' ') << setw(2)  <<"|"<< setfill(' ') << setw(20) <<"Polinomio|"<< endl;
     cout << "--------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
 
     //iterates over queue containing file lines. Prints number of line and how many elemental operations are detected in each line
     for (int i = 0; i < cont; i++)
     {
         cout <<setfill(' ') << setw(6) << i + 1 <<setfill(' ') << setw(7)<< "|" 
-        << setfill(' ') << setw(maxLinea+10) << vecComp.at(i) <<"|"<< setfill(' ') << setw(10) << contOE.at(i)<< setfill(' ') << setw(10) <<"|"<< endl;
+        << setfill(' ') << setw(maxLinea+10) << vecComp.at(i) <<"|"<< setfill(' ') << setw(4) << contOE.at(i)<< setfill(' ') << setw(2) <<"|"<< endl;
     }
 
     cout << "--------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
