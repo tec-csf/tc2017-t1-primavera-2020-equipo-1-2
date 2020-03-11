f3faf344cbea537ebbec8acfdcfece7b1afccbc4
Consgomez <a01026717@itesm.mx>
2020-03-10 23:12:14 -0600

    a ver si asi

---
+++ examples/fors.cpp
@@ -0,0 +1,19 @@
+// La sentencia es la que estaremos utilizando 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;
+
+    k=10;
+
+    for(int i=0; i<k; i++){
+        for(int j=1; j<k+1; j++){
+            cout<<"hola"<<endl;
+        }
+    }
+    
+    return 0;
+}
